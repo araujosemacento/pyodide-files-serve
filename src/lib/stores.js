72a2a1e06181232fdcdefd144f81eedd589181ba
araujosemacento <araujosemacento@users.noreply.github.com>
2025-07-07 06:19:06 +0000

    deploy: f303ab4e97e0bffa84aabdae0c713330b9949520

--- src/lib/stores.js
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const currentPath = writable('/');
-export const files = writable([]);
-export const config = writable({
-  title: 'Servidor de Arquivos Pyodide',
-  description: 'Navegue pelos pacotes Pyodide disponíveis',
-  baseUrl: ''
-});
